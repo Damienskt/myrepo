3cf1a3fdde20893f5b17a2d35274ba5b78503728
Damien Sim <damien_SIM@tech.gov.sg>
2022-11-24 20:24:55 +0800

    work in progress

--- workshop01/client/src/App.js
+++ workshop01/client/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import manu from "./manu.png";
+import logo from "./manu.png";
 import "./App.css";
 
 function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("/message")
+    fetch("http://localhost:3001/api/message")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
 
   return (
-    <div className="App">
-        <img src={manu} alt='manu' width="100" height="100" />
+    <div className="App" style={{border: '3px solid black'}}>
+        <img src={logo} alt="logo" width="200" />
         <b>{!data ? "Loading..." : data}</b><br/>
         <b>Repository <a href="https://github.com/Damienskt/myrepo">https://github.com/Damienskt/myrepo</a></b>
     </div>
